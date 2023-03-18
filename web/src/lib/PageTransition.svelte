d76901995e22b06b00ff7197bea61bbee730425d
thegamerx1 <34986564+thegamerx1@users.noreply.github.com>
2023-03-18 22:09:07 +0100

    Change page transition

--- web/src/lib/PageTransition.svelte
+++ web/src/lib/PageTransition.svelte
@@ -1,14 +1,14 @@
 <script>
-	import { fly } from 'svelte/transition';
+	import { scale } from 'svelte/transition';
 	export let url = '';
 	const pageTransitionDuration = 500;
 </script>
 
 {#key url}
 	<div
-		class="contents"
-		in:fly={{ x: -5, duration: pageTransitionDuration, delay: pageTransitionDuration }}
-		out:fly={{ x: 5, duration: pageTransitionDuration }}
+		class="h-full"
+		in:scale={{ duration: pageTransitionDuration, delay: pageTransitionDuration }}
+		out:scale={{ duration: pageTransitionDuration }}
 	>
 		<slot />
 	</div>
